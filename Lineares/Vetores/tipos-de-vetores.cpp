e56cc742494d029cfb95e12532de857d7e5a6609
mateusribeirocampos <mateusrcampos@gmail.com>
2024-04-15 15:31:38 -0300

    using namespace removido

--- Lineares/Vetores/tipos-de-vetores.cpp
+++ Lineares/Vetores/tipos-de-vetores.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-using namespace std;
+#include <memory>
 
 struct Pessoa {
-    string nome;
+    std::string nome;
     int idade;
 };
 
 int main () {
     // vetor criado com 0 elementos
-    vector<int> vint;
+    std::vector<int> vetor;
 
     // capacity() retorna o tamanho do vetor
-    int espaco = vint.capacity();
+    int espaco = vetor.capacity();
 
     // vetor criado com 10 elementos de valor 0
-    vector<int> ovi(10,0);
+    std::vector<int> ovi(10,0);
 
     // vetor do tipo string
-    vector<string> umaLista;
+    std::vector<std::string> umaLista;
 
     // vetor de vetor
-    vector<vector<int>> matriz;
+    std::vector<std::vector<int>> matriz;
 
     //vetor de ponteiros
-    vector<int*> ponteiros;
+    std::vector<int*> ponteiros;
 
     // Vetor de Objetos Personalizados: Você pode criar vetores de seus próprios tipos de objetos personalizados.
-    vector<Pessoa> pessoas;
+    std::vector<Pessoa> pessoas;
 
 
     // Vetor de Objetos Inteligentes: Você pode usar classes de contêineres inteligentes, como std::unique_ptr ou std::shared_ptr, para armazenar objetos em um vetor, fornecendo gerenciamento automático de memória
-    vector<unique_ptr<Pessoa>> pessoas;
+    std::vector<std::unique_ptr<Pessoa>> pessoas_inteligentes;
 
     // Vetor Constante (const vector): Vetores que não podem ser modificados após a inicialização.
-    const vector<int> vetorConstante {1, 2, 3, 4, 5};
+    const std::vector<int> vetorConstante {1, 2, 3, 4, 5};
 
     // Vetor de tamanho dinâmico: ocê pode criar um vetor cujo tamanho é determinado em tempo de execução.
     int tamanho;
-    cout << "Digite o tamanho do vetor: ";
-    cin >> tamanho;
-    vector<int> vetorDinamico(tamanho);
+    std::cout << "Digite o tamanho do vetor: ";
+    std::cin >> tamanho;
+    std::vector<int> vetorDinamico(tamanho);
 
     return 0;
 }
