10dd46376c86d96f39920912c9f12aa527b86761
mateusribeirocampos <mateusrcampos@gmail.com>
2024-07-06 19:51:29 -0300

    feat: From 2PC

--- Lineares/03lista-encadeada/01lista-encadeada-estatica/01lista-encadeada-estatica-C/delElemList.c
+++ Lineares/03lista-encadeada/01lista-encadeada-estatica/01lista-encadeada-estatica-C/delElemList.c
@@ -2,21 +2,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool delElemList(LinkedList *list, KeyType key) {
-    int previous = INVALID;
-    int i = list->start;
-    while (i != INVALID && list->array[i].rec.key < key) {
-        previous = i;
-        i = list->array[i].next;
-    }
-    if (i == INVALID || list->array[i].rec.key != key) {
-        return false;
-    }
-    if (previous == INVALID) {
-        list->start = list->array[i].next;
-    } else {
-        list->array[previous].next = list->array[i].next;
-    }
-    returnNode(list, i);
-    return true;
+bool delElemList(LinkedList *list, KeyType key)
+{
+  int previous = INVALID;
+  int i = list->start;
+  while (i != INVALID && list->array[i].rec.key < key)
+  {
+    previous = i;
+    i = list->array[i].next;
+  }
+  if (i == INVALID || list->array[i].rec.key != key)
+  {
+    return false;
+  }
+  if (previous == INVALID)
+  {
+    list->start = list->array[i].next;
+  }
+  else
+  {
+    list->array[previous].next = list->array[i].next;
+  }
+  returnNode(list, i);
+  return true;
 }
